a9c6799a725438259dcb68e09de798303ed9cfed
nadeeth <nadeeth@nadeeth.info>
2014-04-13 00:07:38 +0800

    pinch zoom plugin improved

--- www/lib/pinch_zoom.js
+++ www/lib/pinch_zoom.js
@@ -1,10 +1,12 @@
 !function($) {
 
     $.fn.pinchZoom = function() {
+
         var width = $(this).width();
         var prev_distance = 0;
+
         $(this).on("touchmove", function (event) {
-            
+
             //check for two fingers touching
             if (event.originalEvent.touches.length === 2) {
 
@@ -13,21 +15,23 @@
                 var ya = event.originalEvent.touches[0].pageY;
                 var xb = event.originalEvent.touches[1].pageX;
                 var yb = event.originalEvent.touches[1].pageY;
-                
+
                 //Distance between two touches
                 var cur_distance = Math.sqrt(((xa - xb)*(xa - xb)) + ((ya-yb)*(ya-yb)));
-                
+
                 if (prev_distance) {
+
                     if (cur_distance > prev_distance) {//Zoom In
                         width += 10;
                     }
                     if (cur_distance < prev_distance) {//Zoom Out
                         width -= 10;
                     }
-                    
+
+                    //Keep the higth auto to preserve the original aspect ratio
                     $(this).css('background-size', 'auto ' + width + 'px');
                 }
-                
+
                 prev_distance = cur_distance;
             }
         });
