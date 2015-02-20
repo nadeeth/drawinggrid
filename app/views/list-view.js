b27bc7ca93f6fea943ef16d2104777c40e1d0e86
Nadeeth <nadeeth@saltinteractive.com>
2015-02-20 16:40:13 +1300

    populate the form with current grid saved

--- app/views/list-view.js
+++ app/views/list-view.js
@@ -15,19 +15,32 @@ var app = app || {};
 
         // Bind to the relevant events at intialization 
         initialize: function () {
+			this.renderCurrentGrid();
 			//{reset: true} - To suppresses 'add' events for intial loading 
 			app.list.fetch({reset: true});
+        },
+
+        renderCurrentGrid: function () {
+			
+			var grid = localStorage.getItem('current_grid');
+			var picker_color = 'ff0000';
+			
+			if (grid) {
+				grid = new app.Grid(JSON.parse(grid));
+				$("#rows").val(grid.get("rows"));
+                $("#cols").val(grid.get("cols"));
+                //$("#img").val()
+                $("#color_code").val(grid.get("color"));
+				picker_color = grid.get("color");
+			}
+			
 			$('#color').minicolors({//Initialize the color picker
 				theme: 'bootstrap',
-				defaultValue: 'ff0000',
+				defaultValue: picker_color,
 				change: function(hex, opacity) {
 					$('#color_code').val(hex);
 				}
 			});
-        },
-
-        renderCurrentGrid: function () {
-			var grid = localStorage.getItem('current_grid');
         },
 		
 		// Get the list of attributes for a new item
