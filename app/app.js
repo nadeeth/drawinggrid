976211ea1cd21ae01d069f1bf8ee7263c0ef1c99
nadeeth <nadeeth@gmail.com>
2015-04-10 22:35:54 +1200

    screen awake rnds

--- app/app.js
+++ app/app.js
@@ -13,5 +13,13 @@ var ESC_KEY = 27;
         $.slidebars();
 		new app.HomeView();
 		new app.ListView();
+        
+        //Control screen sleep
+//        var onDeviceReady = function(){
+//            alert('hi');
+//            alert(keepscreenon);
+//            keepscreenon.enable();
+//        };
+//        document.addEventListener('deviceready', onDeviceReady, false);
     });
 })(jQuery);
