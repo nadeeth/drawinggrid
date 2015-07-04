f5561476c2729a28acb95115d0802a2f1ba09c7f
nadeeth <nadeeth@gmail.com>
2015-07-04 19:39:06 +1200

    Filters

--- app/views/filters-view.js
+++ app/views/filters-view.js
@@ -18,59 +18,49 @@ var app = app || {};
             $('.dg-filter').click(function(){
                 
                 //VintageJS
-                if ($(this).data('dg-filter') === 'filter-sepia') {
-                    var img = document.getElementById('active-grid-img');
-                    var options = {
-                        onError: function() {
-                            alert('Filter Error.');
-                        },
-                        onStop: function() {
-                            
-                            //this.save_filtered_image($('#active-grid-img').attr('src'));
-                            var grid = JSON.parse(localStorage.getItem('current_grid'));
-                            grid.filter = true;
-                            localStorage.setItem('current_grid', JSON.stringify(grid));
-                            
-                            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+                   
+                var img = document.getElementById('active-grid-img');
 
-                            function gotFS(fileSystem) {
-                                fileSystem.root.getFile("grid_filtered.txt", {create: true, exclusive: false}, gotFileEntry, fail);
-                            }
+                var options = {
+                    onError: function() {
+                        alert('Filter Error.');
+                    },
+                    onStop: function(img) {
 
-                            function gotFileEntry(fileEntry) {
-                                fileEntry.createWriter(gotFileWriter, fail);
-                            }
+                        var grid = JSON.parse(localStorage.getItem('current_grid'));
+                        grid.filter = true;
+                        localStorage.setItem('current_grid', JSON.stringify(grid));
 
-                            function gotFileWriter(writer) {
-                                writer.write($('#active-grid-img').attr('src'));
-                            }
+                        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
 
-                            function fail(error) {
-                                alert('write-error - '+error.code);
-                            }
+                        function gotFS(fileSystem) {
+                            fileSystem.root.getFile("grid_filtered.txt", {create: true, exclusive: false}, gotFileEntry, fail);
                         }
-                    };
-                    var effect = {
-                        vignette: 0.6,
-                        sepia: true
-                    };
-                    new VintageJS(img, options, effect);
-                }
-                
+
+                        function gotFileEntry(fileEntry) {
+                            fileEntry.createWriter(gotFileWriter, fail);
+                        }
+
+                        function gotFileWriter(writer) {
+                            writer.write($('#active-grid-img').attr('src'));
+                        }
+
+                        function fail(error) {
+                            alert('write-error - '+error.code);
+                        }
+                    }
+                };
+
                 if (!$(this).data('dg-filter')) {
                     var grid = JSON.parse(localStorage.getItem('current_grid'));
                     grid.filter = '';
                     localStorage.setItem('current_grid', JSON.stringify(grid));
+                } else {
+                    new VintageJS(img, options, vintagePresets[$(this).data('dg-filter')]);
                 }
-
 
                 new app.HomeView();
             });
-        },
-        
-        save_filtered_image: function(img) {alert('cxhi');//TODO: refactor
-        
-            
         }
     });
 })(jQuery);
