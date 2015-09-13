df80f69c74927a653fdb567a2cb5dccf4de72f58
nadeeth <nadeeth@gmail.com>
2015-09-13 14:24:52 +1200

    app config

--- www/app/app.js
+++ www/app/app.js
@@ -2,20 +2,35 @@ var app = app || {};
 var ENTER_KEY = 13;
 var ESC_KEY = 27;
 
-document.addEventListener(
-    'deviceready', 
-    function() {
-        'use strict';
-        $.slidebars();
-        new app.HomeView({show_loading_graphic:true});
-        new app.ListView();
-        new app.FiltersView();
-        keepscreenon.enable();//Keep Screen Awake.
-    },
-    false
-);
-
+document.addEventListener('deviceready',app_init,false);
 document.addEventListener("backbutton", onBackKeyDown, false);
+
+$(document).ready(function(){
+    if (DG_Conf.mode === 'web') {
+       app_init();
+    }
+    if (DG_Conf.testmode) {
+        load_tests();
+    }
+    if (DG_Conf.debugmode) {//weinre debuging
+        var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.src = DG_Conf.debughost + "/target/target-script-min.js#" + DG_Conf.debugid;
+        $("head").append(s);
+    }
+});
+
+function app_init() {
+    'use strict';
+    $.slidebars();
+    new app.HomeView({show_loading_graphic:true});
+    new app.ListView();
+    new app.FiltersView();
+    if (DG_Conf.mode !== 'web') {
+        keepscreenon.enable();//Keep Screen Awake.
+    }
+}
+
 function onBackKeyDown() {
     if ($('.sb-active').length) {
         $('.sb-close').click();
@@ -24,3 +39,27 @@ function onBackKeyDown() {
     }
 }
 
+function load_tests() {
+    var jasmine_files = [
+        //Jasmine Library
+        'lib/jasmine-2.3.4/jasmine.js',
+        'lib/jasmine-2.3.4/jasmine-html.js',
+        'lib/jasmine-2.3.4/boot.js',
+        //Source files
+        'src/Player.js',
+        'src/Song.js',
+        //Spec files
+        'spec/SpecHelper.js',
+        'spec/PlayerSpec.js'
+    ];
+    for (var i=0; i < jasmine_files.length; i++) {
+        var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.src = jasmine_files[i];
+        $("head").append(s);
+    }
+    var s = document.createElement("link");
+    s.rel = "stylesheet";
+    s.href = "lib/jasmine-2.3.4/jasmine.css";
+    $("head").append(s);
+}
---
+++ www/app/config.js
@@ -0,0 +1,8 @@
+//App Config file
+var DG_Conf = {
+    mode : 'web',//options: web, mobile (default - mobile)
+    testmode : true,
+    debugmode: false,//weinre debug configuration
+    debughost : 'http://192.168.1.4:8080',
+    debugid : 'nadeeth'
+};
